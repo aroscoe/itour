4468a80d8b8f71b38ec5af50316877ce55209874
Anthony Roscoe <me@anthonyroscoe.com>
2010-10-15 15:07:03 -0400

    returning concerts

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import sys
 import os.path
 import plistlib
 
+import bandsintown
+
+from settings import APP_ID
+
+bandsintown.app_id = APP_ID
+
 def main(file):
     if os.path.exists(file):
-        print get_artists(file)
+        artists = get_artists(file)
+        
+        concerts = []
+        for i in range(0, len(artists), 50):
+            new_concerts = bandsintown.Event.search(artists=artists[i:i+50],
+                radius=10, per_page=10, location="Brooklyn, NY")
+            if new_concerts:
+                concerts += new_concerts
+        print concerts
 
 def get_artists(file):
     '''Reads itunes xml file and extracs artists'''
@@ -15,10 +29,14 @@ def get_artists(file):
     # Get all artists
     all_artists = []
     for track in tracks.values():
-        all_artists.append(track.get("Artist"))
+        artist = track.get("Artist")
+        if artist:
+            artist = artist.encode("utf8")
+            all_artists.append(artist)
     
     # Remove duplicates
     artists = []
+    # TODO: use a Set
     for artist in sorted(all_artists):
         if (artists == [] or artist != artists[-1]) and artist != None:
             artists.append(artist)
