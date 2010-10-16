59b26040e904673d87ea5b94445d22c78946cf96
Anthony Roscoe <me@anthonyroscoe.com>
2010-10-16 11:37:15 -0400

    added test data

--- main.py
+++ main.py
@@ -5,40 +5,39 @@ import os.path
 from optparse import OptionParser
 
 import itour
+import test
 
 def main():
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="filename", help="read data from FILENAME")
+    parser.add_option("-t", "--test",
+                      action="store_true", dest="test_mode",
+                      help="Use test data set instead of parsing iTunes XML file")
     (options, args) = parser.parse_args()
     if len(args) > 0:
         parser.error("incorrect number of arguments")
     
-    if options.filename:
+    if options.test_mode:
+        artists = test.artists
+    elif options.filename:
         if os.path.exists(options.filename):
             artists = itour.get_artists(options.filename)
-            
-            print "\n%d Artists\n" % len(artists)
-            
-            artists = artists[:50]
-            
-            print "\n\n\n\n"
-            
-            print artists[:50]
-            
-            print "\n\n\n\n"
-            
-            concerts = itour.get_concerts(artists)
-            
-            if concerts:
-                print concerts
-                print "\n\n %d Concerts" % len(concerts)
-            else:
-                print "::: NO CONCERTS :::"
         else:
-            error = "File: %s does not exist." % options.filename
+            error = "File: %s does not exist" % options.filename
             parser.error(error)
     else:
-        parser.error("You must specify iTunes XML file (-f).")
+        parser.error("You must specify iTunes XML file (-f)")
+    
+    if artists:
+        print "\n%d Artists\n" % len(artists)
+        
+        concerts = itour.get_concerts(artists)
+        
+        if concerts:
+            print concerts
+            print "\n\n %d Concerts" % len(concerts)
+        else:
+            print "::: NO CONCERTS :::"
 
 if __name__ == "__main__":
     main()
